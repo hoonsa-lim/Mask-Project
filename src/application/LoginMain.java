b88a1e9ccff451500fcd7e2950486b5aef6650e9
myohoon95 <myohoon95@gmail.com>
2020-06-30 09:34:55 +0900

    byk login main, controller

---
+++ src/application/LoginMain.java
@@ -0,0 +1,30 @@
+package application;
+
+
+import controller.Login_controller;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LoginMain extends Application {
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxmlLoder = new FXMLLoader(getClass().getResource("/view/login.fxml"));
+		Parent root = fxmlLoder.load();
+		Login_controller loginController = fxmlLoder.getController();
+		loginController.stage = primaryStage;
+		
+		Scene scene = new Scene(root);
+		
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+}
